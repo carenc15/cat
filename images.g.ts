279d763116207f3bb282f9b7880f05a3998a6157
carenc15 <157772283+carenc15@users.noreply.github.com>
2024-01-25 20:25:27 -0600

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,63 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "Purple cat":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . a . . a . . . . . . . . . . 
+. . a a a a . . . . . . . a . . 
+. . 7 a a 7 . . . . . . a a . . 
+. . a 3 a a . . . . . a a . . . 
+. . a a a a a a a a a a . . . . 
+. . . . . a a a a a . . . . . . 
+. . . . . a a a a a . . . . . . 
+. . . . . a a a a a . . . . . . 
+. . . . . a a . a a . . . . . . 
+. . . . a a . . . a a . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image2":
+            case "Cat food":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . 7 e . . . . . . . . 
+. . . . . . e e e . . . e . . . 
+. . . . f e e e e e f . . . . . 
+. . e . f f f f f f f . . . . . 
+. . . . f f f f f f f . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
---
+++ main.ts
@@ -0,0 +1,4 @@
+let mySprite = sprites.create(assets.image`Purple cat`, SpriteKind.Player)
+let mySprite2 = sprites.create(assets.image`Cat food`, SpriteKind.Food)
+controller.moveSprite(mySprite)
+scene.setBackgroundColor(8)
